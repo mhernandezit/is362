50577f24ef9c9dd85a4acb6b87623b7082934868
mhernandezit <mhernandezit@gmail.com>
2019-01-29 19:44:53 -0500

    final revision - adding questions/answers

--- assignment_1.sql
+++ assignment_1.sql
@@ -11,6 +11,13 @@ FROM   planes
 WHERE  speed IS NOT NULL;
 
 /*
+Answer: 
+The minimum speed of an airplane is 90
+The maximum speed of an airplane is 432
+23 airplanes have listed speeds
+*/
+
+/*
 2. What is the total distance flown by all of the planes in January 2013?  
 What is the total distance flown by all of the planes in January 2013 where the tailnum is missing?
 */
@@ -29,6 +36,12 @@ WITH fullsum AS
 SELECT     * 
 FROM       fullsum 
 INNER JOIN nullsum;
+
+/*
+Answer: 
+The total distance flown by all planes in January 2013 is 27188805
+The distance flown for planes that have no tailnums is 81763
+*/
 
 /*
 3. What is the total distance flown for all planes on July 5, 2013 grouped by aircraft manufacturer?  
@@ -59,6 +72,45 @@ GROUP  BY planes.manufacturer
 ORDER  BY planes.manufacturer; 
 
 /*
+Answer: 
+Here is the query results:
+# Sum(flights.distance), manufacturer
+195089, AIRBUS
+78786, AIRBUS INDUSTRIE
+2199, AMERICAN AIRCRAFT INC
+937, BARKER JACK L
+335028, BOEING
+31160, BOMBARDIER INC
+1142, CANADAIR
+2898, CESSNA
+1089, DOUGLAS
+77909, EMBRAER
+1157, GULFSTREAM AEROSPACE
+7486, MCDONNELL DOUGLAS
+15690, MCDONNELL DOUGLAS AIRCRAFT CO
+4767, MCDONNELL DOUGLAS CORPORATION
+
+The query with the left join counts the distance of planes that do not have a manufacturer field:
+
+# Sum(flights.distance), manufacturer
+127671, 
+195089, AIRBUS
+78786, AIRBUS INDUSTRIE
+2199, AMERICAN AIRCRAFT INC
+937, BARKER JACK L
+335028, BOEING
+31160, BOMBARDIER INC
+1142, CANADAIR
+2898, CESSNA
+1089, DOUGLAS
+77909, EMBRAER
+1157, GULFSTREAM AEROSPACE
+7486, MCDONNELL DOUGLAS
+15690, MCDONNELL DOUGLAS AIRCRAFT CO
+4767, MCDONNELL DOUGLAS CORPORATION
+*/
+
+/*
 4. Is there a correlation between temperature and delays?  Is there a higher average delay in the summer or winter months?
 Are there any months which have a negative delay (more on time flights than delayed)?
 Export the result set to CSV
@@ -83,3 +135,8 @@ INTO OUTFILE 'C:/Users/Praetor/OneDrive - CUNY School of Professional Studies/is
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'; 
+/*
+There is not a direct correlation with average delay to temperature - from the data it looks like the highest average delays are 
+in the winter and summer months.  Interestingly, the months of May, August and September have significantly lower average delays
+than the other months.  It would make more sense to travel through these months to avoid delays.
+*/
